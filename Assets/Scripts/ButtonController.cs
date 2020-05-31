90a7553be77bd1c6ab0780e52e1bd6c1222c104a
jedrek <jedrekszor@wp.pl>
2020-05-31 16:38:16 +0100

    working mechanics

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+    public int id;
+    public Color color;
+    private Image button; 
+    
+    void Start()
+    {
+        button = transform.GetComponentInParent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setColor(bool set)
+    {
+        if (set)
+        {
+            button.color = color;
+        }
+        else
+        {
+            button.color = Color.white;
+        }
+    }
+}
+
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public float blinkTime;
+    public float noBlinkTime;
+    public float byczqTime;
+
+    private List<ButtonController> buttons = new List<ButtonController>();
+    private List<int> order = new List<int>();
+    private int currentButton;
+    private int buttonCount = 0;
+
+    private bool blinkCd = true;
+    private bool sequenceComplete = false;
+    private bool awaitingInput = false;
+
+    private bool canBlink = true;
+    private bool canNoBlink = true;
+
+    private TextMeshProUGUI tm;
+
+    void Start()
+    {
+        foreach (Transform button in GameObject.Find("Buttons").transform)
+        {
+            buttons.Add(button.GetComponent<ButtonController>());
+        }
+
+        tm = GameObject.Find("Feedback").GetComponent<TextMeshProUGUI>();
+        tm.enabled = false;
+        addToOrder();
+        currentButton = order.ElementAt(0);
+    }
+
+    void Update()
+    {
+        if (!awaitingInput && buttonCount >= order.Count)
+        {
+            sequenceComplete = true;
+            awaitingInput = true;
+            buttonCount = 0;
+        }
+
+        if (!sequenceComplete)
+        {
+            currentButton = order.ElementAt(buttonCount);
+            if (blinkCd)
+            {
+                if (canBlink)
+                    StartCoroutine(blink());
+            }
+            else
+            {
+                if (canNoBlink)
+                    StartCoroutine(noBlink());
+            }
+        }
+    }
+
+    private void addToOrder()
+    {
+        order.Add(Random.Range(0, buttons.Count));
+    }
+
+    private IEnumerator blink()
+    {
+        canBlink = false;
+        buttons.ElementAt(currentButton).setColor(true);
+        yield return new WaitForSeconds(blinkTime);
+        blinkCd = false;
+        canBlink = true;
+    }
+
+    private IEnumerator noBlink()
+    {
+        canNoBlink = false;
+        buttons.ElementAt(currentButton).setColor(false);
+        buttonCount++;
+        yield return new WaitForSeconds(noBlinkTime);
+        blinkCd = true;
+        canNoBlink = true;
+    }
+
+    public void onClick(ButtonController b)
+    {
+        if (sequenceComplete && awaitingInput)
+        {
+            if (b.id == order.ElementAt(buttonCount))
+            {
+                buttonCount++;
+                if (buttonCount >= order.Count)
+                {
+                    StartCoroutine(ojTakByczq());
+                }
+            }
+            else
+            {
+                StartCoroutine(ojNieByczQ());
+            }
+        }
+    }
+
+    private IEnumerator ojTakByczq()
+    {
+        yield return new WaitForSeconds(byczqTime);
+        addToOrder();
+        buttonCount = 0;
+        sequenceComplete = false;
+        awaitingInput = false;
+    }
+
+    private IEnumerator ojNieByczQ()
+    {
+        tm.enabled = true;
+        yield return new WaitForSeconds(byczqTime);
+        tm.enabled = false;
+        order.Clear();
+        addToOrder();
+        buttonCount = 0;
+        sequenceComplete = false;
+        awaitingInput = false;
+    }
+}
